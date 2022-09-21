0b94a23517a778b86ea3aab1890f052d14a0a738
TaDangLam <tadanglam93@gmail.com>
2022-09-21 16:46:50 +0700

    Dinh nghia cac routes cho MVC model

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('express');
 const app = express("cors");
+const contactsRouter = require("./app/routes/contact.routes");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({message: "Welcome to contact book application."}));
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
